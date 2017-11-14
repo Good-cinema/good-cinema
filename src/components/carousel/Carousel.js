25c2ef58f0be9f28ce14cda79d642c094b68b371
Nickolas Tenney <ntenney@spillman.com>
2017-11-14 09:23:53 -0700

    Added notes

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,20 +1,27 @@
+// React Slick Slider
 import React, { Component } from 'react';
 import Slider from 'react-slick';
+// This is the default CSS for the carousel.
 import "../../../node_modules/slick-carousel/slick/slick.css";
 import "../../../node_modules/slick-carousel/slick/slick-theme.css";
+// This is the CSS I made that overrides the default CSS for certain settings.
 import "./carousel.css";
 
+// This is the slider component.
 class SimpleSlider extends Component {
   render() {
+    // This is where you add the settings for the slider itself. There are many options.
     var settings = {
       centerMode: true,
       centerPadding: "20px",
+      dots: true,
       speed: 500,
       slidesToShow: 3,
       slidesToScroll: 1,
       infinite: true
     };
     return (
+      // This is where the slider and the settings themselves are initiated along with the actual slides.
       <Slider {...settings}>
           <div className="fpmain">
             <img src={ require("../../img/ptposter.jpg") } 
