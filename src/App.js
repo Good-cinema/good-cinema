9bbf45759cdf0498104c0d233ed971a01e859725
tenndawg <nickolas.tenney@gmail.com>
2017-11-14 20:09:26 -0700

    Adding movie component and added WatchlistButton component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import SimpleSlider from './components/carousel/Carousel';
+import Movie from './components/movie/Movie';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <SimpleSlider />
+        <Movie />
       </div>
     );
   }
