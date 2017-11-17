24a9827ca533caa9f785417eed182a8300b7a43a
tenndawg <nickolas.tenney@gmail.com>
2017-11-17 15:31:23 -0700

    Getting the css down

--- src/components/movie/Movie.js
+++ src/components/movie/Movie.js
@@ -1,29 +1,18 @@
 import React, { Component } from 'react';
 import './movie.css';
-import StarRatingComponent from 'react-star-rating-component';
 import WatchlistButton from '../watchlistButton/WatchlistButton';
 import MovieOverview from '../movieAPI/MovieOverview';
 import MovieTitle from '../movieAPI/MovieTitle';
 import MoviePoster from '../movieAPI/MoviePoster';
+import MovieRating from '../movieAPI/MovieRating';
 
 export default class Movie extends Component {
-    constructor() {
-        super();
- 
-        this.state = {
-            rating: 1
-        };
-    }
- 
-    onStarClick(nextValue, prevValue, name) {
-        this.setState({rating: nextValue});
-    }
+
     render() {
-        const { rating } = this.state;
         return(
             <div className="movieBody equalHMRWrap">
                 <span className="box equalHMR"><h1><MovieTitle /></h1></span>
-                <span className="box equalHMR marginTop">
+                <span className="box equalHMR marginBottom">
                     <MoviePoster />
                     {/* <img src={ require("../../img/ramboposter.jpg") } 
                     alt="Rambo Poster" 
@@ -32,19 +21,13 @@ export default class Movie extends Component {
                 <span className="box  equalHMR marginBottom">
                     <WatchlistButton />
                 </span>
-                <span style={{ fontSize: 34 }} className="box equalHMR">
-                    <StarRatingComponent 
-                        name="rate1" 
-                        starCount={5}
-                        value={rating}
-                        onStarClick={this.onStarClick.bind(this)}
-                    />
-                    <button className="reviewButton">Review</button>
+                <span className="box equalHMR marginLeft marginTop">
+                    <MovieRating />
                 </span>
-                <span className="box equalHMR marginTop overview">
+                <span className="box equalHMR overview marginLeft negativeMarginTop negMarginBottom">
                     <MovieOverview />
                 </span>
-                <span className="box equalHMR">
+                <span className="box equalHMR marginLeft">
                     <img src={ require("../../img/Amazon-Instant-Video-banner.jpg") } 
                     alt="Amazon Banner" 
                     style={{width: 330, height: 125}} />
---
+++ src/components/movieAPI/MovieRating.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import StarRatingComponent from 'react-star-rating-component';
+import "../movie/movie.css";
+
+export default class MovieRating extends Component {
+    constructor() {
+        super();
+        
+        this.state = {
+            rating: 1
+        };
+}
+
+    onStarClick(nextValue, prevValue, name) {
+        this.setState({rating: nextValue});
+    }
+
+    componentDidMount() {
+        fetch('https://api.themoviedb.org/3/movie/1368?api_key=c1518c5dfeb80ddb9ed594cba690acaf&language=en-US')
+        .then(response => {
+            if (!response.ok) {
+                throw Error("Network request failed")
+              }
+            return response;
+        })
+        .then(data => data.json())
+        .then(data => {    
+            this.setState({
+                movieAPI: data
+            })
+    }, () => {
+        this.setState({
+          requestFailed: true
+        })
+      })
+    }
+
+    render() {
+        const { rating } = this.state;
+        if (this.state.requestFailed) return <p>Failed!</p>
+        if (!this.state.movieAPI) return <p>Loading...</p>
+        return (
+            <div>
+                <span className="avgBlock" style={{ fontSize: 22 }}>Average Rating {this.state.movieAPI.vote_average/2} Stars</span>
+                <span className="starBlock" style={{ fontSize: 34}}>
+                    <StarRatingComponent 
+                            name="rate1" 
+                            starCount={5}
+                            value={rating}
+                            onStarClick={this.onStarClick.bind(this)}
+                        />
+                    <button className="reviewButton">Review</button>
+                </span>
+            </div>
+        )
+    }
+
+
+
+}
