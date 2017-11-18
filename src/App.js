4e536c345e714d834bfd5448603072244a48f00c
tenndawg <nickolas.tenney@gmail.com>
2017-11-18 10:04:31 -0700

    merge conflicts resolved

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React, { Component } from 'react';
 import './App.css';
 import SimpleSlider from './components/carousel/Carousel';
 import Movie from './components/movie/Movie';
-import MovieSearch from './components/movieAPI/MovieSearch';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <MovieSearch />
+        <Movie />
       </div>
     );
   }
