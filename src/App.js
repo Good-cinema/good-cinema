c24ae9cc6fae32df38c69b0281ff99d7b00d7656
Kevin Patterson <Home@Kevins-MacBook.local>
2017-11-18 21:45:32 -0700

    This is the Home page with routing, Sign In and Reg button link to next pages

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import HomePage from './components/homePage/HomePage';
 import SimpleSlider from './components/carousel/Carousel';
 import Movie from './components/movie/Movie';
-// import SiteFrame from './Components/SiteFrame/SiteFrame';
+import SiteFrame from './components/SiteFrame/SiteFrame';
 // import HomePage from './Views/HomePage/HomePage';
 
 import './App.css';
@@ -19,6 +19,8 @@ class App extends Component {
             <Route path='/SimpleSlider' component={ SimpleSlider }/>
             {/* This is the path to the "Movie" */}
             <Route path='/Movie' component={ Movie }/>
+            {/* This is the path to the "SiteFrame" */}
+            <Route path='/SiteFrame' component={ SiteFrame } />
             {/* Root or Home Router should alway be place at bottom to filter
             out bad links. "exact" is not used to improve the filter of links entered */}
             <Route path='/' component={ HomePage } />
--- src/Components/SiteFrame/SiteFrame.js
+++ src/Components/SiteFrame/SiteFrame.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './../../App.css';
 import Header from './../Header/Header';
 import Footer from './../Footer/Footer';
+import Movie from '../movie/Movie';
 import './SiteFrame.css';
 import ActivityFeed from './../ActivityFeed/ActivityFeed';
 
@@ -12,6 +13,9 @@ class SiteFrame extends Component {
                 <Header/>
                 <main className='container'>
                     {this.props.children}
+                    <div>
+                        <Movie />
+                    </div>
                 </main>
                 <Footer/>
             </div>
