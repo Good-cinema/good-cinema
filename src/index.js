0f4bbd96b796c04ab1847115c33c843cf0468188
tenndawg <nickolas.tenney@gmail.com>
2017-11-21 19:34:44 -0700

    htsgfd

--- src/index.js
+++ src/index.js
@@ -2,14 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-<<<<<<< HEAD
 
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-=======
-
-import './index.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
->>>>>>> master
